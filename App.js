b5aae5b9b6bf76bae8147f91681fdd58865ffed2
Seikku <bgu221@myy.haaga-helia.fi>
2023-01-16 15:33:31 +0200

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput  } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [msg, setMsg] = React.useState();
+
+  const buttonPressed = () => {
+    Alert.alert('BUTTON HAS BEEN PRESSED', 'You wrote: ' + msg)
+  }
   return (
     <View style={styles.container}>
-      <Text>mo i</Text>
+        <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        value = {msg}
+        onChangeText = {text => setMsg(text)}
+        />
+      <Button title="press me" onPress={buttonPressed}></Button>
       <StatusBar style="auto" />
     </View>
   );
